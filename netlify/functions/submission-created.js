3fddb80f038f261dfdfbab81f85ce28938fa0aac
Noor E Alam <117395958+noorealamdev@users.noreply.github.com>
2025-05-11 00:01:12 +0600

    Update submission-created.js

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,12 +1,27 @@
+// Capture verified submission form data and send it to multiple locations 
 exports.handler = async (event) => {
-    const formData = JSON.parse(event.body).payload;
-    const { data } = formData;
-    console.log("New Submission:", data);
-    
-    // You can process the data here (save to DB, send emails, etc.)
+  const formData = JSON.parse(event.body).payload;
+  const { data } = formData;
+  console.log("New Submission:", data);
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Submission received!" }),
-    };
+  // Now process the data here (save to DB, send emails, APIs etc.)
+  fetch('http://localhost:8000/api.php', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then(response => response.json())
+    .then(responseData => {
+      console.log('Success:', responseData);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Submission received!" }),
+  };
 };
